a332ae6318e54822ea04abefd707270d4766eb11
森史憲 <mori@codingdesing.jp>
2024-11-18 17:30:55 +0900

    update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,11 @@ export default defineNuxtConfig({
     '@nuxtjs/plausible',
     // https://github.com/nuxt/devtools
     '@nuxt/devtools'
-  ]
+  ],
+
+  compatibilityDate: '2024-11-18',
+
+  devtools: {
+    enabled: true
+  }
 })
